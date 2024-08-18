40d5eeca929c281ed537d2674c4991d9fcb32a48
Aniket Bindhani <aniketbindhani44@gmail.com>
2024-08-18 15:19:04 +0530

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,19 +13,24 @@ const Navbar = () => {
     return (
         <nav className='p-4 md:p-6 shadow-md'>
             <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-                <a className="text-xl font-bold mb-4 md:mb-0" href="#">AnonMessage</a>
-                {
-                    session ? (
-                        <>
-                            <span className='mr-4'>Welcome, {user?.username || user?.email}</span>
-                            <Button className="w-full md:w-auto" onClick={()=> signOut()}>Logout</Button>
-                        </>
-                    ) : (
-                        <Link href="/sign-in">
-                                <Button className="w-full md:w-auto">Login</Button>
-                        </Link>
-                    )
-                }
+                <a className="text-xl font-bold mb-2 md:mb-0 md:order-1" href="#">AnonMessage</a>
+
+                {session ? (
+                    <>
+                        <div className="flex-grow text-center md:text-center md:order-2">
+                            <span className='text-sm md:text-base'>
+                                Welcome, {user?.username || user?.email}
+                            </span>
+                        </div>
+                        <div className="flex md:order-3">
+                            <Button className="w-full md:w-auto text-sm py-1 px-3" onClick={() => signOut()}>Logout</Button>
+                        </div>
+                    </>
+                ) : (
+                    <Link href="/sign-in" className="md:order-2">
+                        <Button className="w-full md:w-auto text-sm py-1 px-3">Login</Button>
+                    </Link>
+                )}
             </div>
         </nav>
     )
